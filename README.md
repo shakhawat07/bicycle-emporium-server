4a3169422a43b9ecbb7b008239cce58ffe203897
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-11-13 13:09:56 +0600

    all api added

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bicycle Emporium
+Live site project link: [Bicycle Emporium](https://tour-de-planet-earth-client.web.app/)
+
+### About Project:
+Bicycle Emporium was opened in October 2010. Back then we were riders. Recreational cycling was something that hadn't yet become mainstream in dhaka, and there were no shop addressing biker's need . Yes, there were shops selling bikes, but with every visit to the local bike store, we realized how discouraging an interaction could be. In time, our frustration became the motivation to open a bike shop which will be honest to customer and their needs, give professional support and service and which will encourage people to try and take up bicycling as a way of life. Our focus was solving the problems that we our self have faced as customer. With the purchase of every bicycle, you will receive 6 months of servicing free. We also have our weekly rides to help you enjoy the whole experience .Our monthly tech sessions are designed to make you more hands on.
+
+### Project Highlights:
+* Bicycle Selling Website
+* You can get our latest bicycle in homepage
+* You can manage your orders
+* You can manage all orders if you log in
+* Our team members are there in the homepage
+* We have a dashboard section which is private
+* We have a about section
+* You can give a review
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const admin = require("firebase-admin");
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
-const cors = require('cors');
-require('dotenv').config();
-
-const app = express();
 const port = process.env.PORT || 5000;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 // middleware
 app.use(cors());
@@ -17,6 +23,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// verify token 
+async function verifyToken(req, res, next) {
+    if (req.headers.authorization.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
+
 async function run() {
     try {
         await client.connect();
@@ -26,9 +49,10 @@ async function run() {
 
         const usersCollection = database.collection('users');
         const teamMembersCollection = database.collection('teamMembers');
-        // const bicycleCategoriesCollection = database.collection('bicycleCategories');
         const ordersCollection = database.collection('orders');
+        const reviewsCollection = database.collection('reviews');
 
+        //--------------------------USERS--------------------------
 
         // GET API OF USERS
         app.get('/users', async (req, res) => {
@@ -37,18 +61,7 @@ async function run() {
             res.send(users);
         });
 
-
-        // app.get('/users/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const query = { email: email };
-        //     const user = await usersCollection.findOne(query);
-        //     let isAdmin = false;
-        //     if (user?.role === 'admin') {
-        //         isAdmin = true;
-        //     }
-        //     res.json({ admin: isAdmin });
-        // })
-
+        // POST API OF USERS
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(req.body);
@@ -57,6 +70,7 @@ async function run() {
             res.json(result);
         });
 
+        // PUT API OF USERS
         app.put('/users', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
@@ -66,6 +80,67 @@ async function run() {
             res.json(result);
         });
 
+        //--------------------------ADMIN--------------------------
+
+        // GET API OF ADMIN
+        app.get('/users/:email', async (req, res) => {
+
+            const email = req.params.email;
+            if (email) {
+                const query = { email: email };
+                const user = await usersCollection.findOne(query);
+                // console.log('this is user', user);
+                let isAdmin = false;
+                if (user) {
+                    if (user.role === 'admin') {
+                        isAdmin = true;
+                    }
+                    res.json({ admin: isAdmin });
+                }
+                else {
+                    res.json({ admin: false });
+                }
+
+            }
+            else {
+                res.json({ admin: false });
+            }
+
+        });
+
+        // PUT API OF ADMIN
+
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const requester = req.decodedEmail;
+            console.log('requester', requester);
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                console.log('requesterAccount', requesterAccount);
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have access to make admin' })
+            }
+
+        });
+
+        // app.put('/users/admin', async (req, res) => {
+        //     const user = req.body;
+        //     console.log('put', user);
+        //     const filter = { email: user.email };
+        //     const updateDoc = { $set: { role: 'admin' } };
+        //     const result = await usersCollection.updateOne(filter, updateDoc);
+        //     res.json(result);
+        // })
+
+        // --------------------------BICYCLES--------------------------
 
         // GET API OF BICYCLES
         app.get('/bicycles', async (req, res) => {
@@ -83,6 +158,26 @@ async function run() {
             res.json(bicycle);
         });
 
+        // POST API BICYCLES
+        app.post('/bicycles', async (req, res) => {
+            const bicycle = req.body;
+            console.log('hit the post api', bicycle);
+
+            const result = await bicyclesCollection.insertOne(bicycle);
+            console.log(result);
+            res.json(result);
+        });
+
+        // DELETE API SINGLE BICYCLE
+        app.delete('/bicycles/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bicyclesCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        // --------------------------TEAM MEMBERS--------------------------
+
         // GET API OF TEAM MEMBERS
         app.get('/teamMembers', async (req, res) => {
             const cursor = teamMembersCollection.find({});
@@ -90,12 +185,25 @@ async function run() {
             res.send(teamMembers);
         });
 
-        // // GET API OF BICYCLE CATEGORIES
-        // app.get('/bicycleCategories', async (req, res) => {
-        //     const cursor = bicycleCategoriesCollection.find({});
-        //     const bicycleCategories = await cursor.toArray();
-        //     res.send(bicycleCategories);
-        // });
+        // --------------------------REVIEWS-------------------------- 
+
+        // GET API OF REVIEWS
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // POST API OF REVIEWS
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log(req.body);
+            const result = await reviewsCollection.insertOne(review);
+            console.log(result);
+            res.json(result);
+        });
+
+        // --------------------------ORDERS--------------------------
 
         // GET API OF ADD ORDERS
         app.get('/orders', async (req, res) => {
@@ -113,16 +221,6 @@ async function run() {
             res.json(order);
         });
 
-        // // POST API BICYCLES
-        // app.post('/bicycles', async (req, res) => {
-        //     const bicycles = req.body;
-        //     console.log('hit the post api', bicycles);
-
-        //     const result = await bicyclesCollection.insertOne(tourCategory);
-        //     console.log(result);
-        //     res.json(result)
-        // });
-
         // POST API ADD ORDERS
         app.post('/orders', async (req, res) => {
             const order = req.body;
@@ -137,6 +235,7 @@ async function run() {
             const result = await ordersCollection.deleteOne(query);
             res.json(result);
         });
+
         //UPDATE API ORDER STATUS
         app.put('/orders/:id', async (req, res) => {
             const id = req.params.id;
@@ -151,7 +250,7 @@ async function run() {
             const result = await ordersCollection.updateOne(filter, updateDoc, options)
             console.log('updating', id)
             res.json(result)
-        })
+        });
 
     }
     finally {
